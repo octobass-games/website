dfc2ae002bade96e4b94fb12c5c6869229cd1d78
majicmoo <megan@theparkes.me.uk>
2020-03-10 21:43:07 +0000

    Updates

---
+++ precache-manifest.8309eac76fc409ceb738a7c64197f77d.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3e34810b3fecbb4422703c96cc06c988",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e1de2ddec07dae70ede5",
+    "url": "/static/css/main.071fe4ef.chunk.css"
+  },
+  {
+    "revision": "4afae64708e98949a998",
+    "url": "/static/js/2.ed0ab875.chunk.js"
+  },
+  {
+    "revision": "e76927ded4e98c8bb3c1b6b2d3f786e8",
+    "url": "/static/js/2.ed0ab875.chunk.js.LICENSE"
+  },
+  {
+    "revision": "e1de2ddec07dae70ede5",
+    "url": "/static/js/main.57e53ae0.chunk.js"
+  },
+  {
+    "revision": "6400a54b5e093ad7b74c",
+    "url": "/static/js/runtime-main.68ba7d76.js"
+  },
+  {
+    "revision": "5b5d45222ba7d231d05d781fed0c4e34",
+    "url": "/static/media/octobass.5b5d4522.svg"
+  },
+  {
+    "revision": "821a0a402168e968bf065b834aeb9935",
+    "url": "/static/media/simple-octobass.821a0a40.svg"
+  }
+]);
