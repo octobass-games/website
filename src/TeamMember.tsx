452d1d6d1c4123ff5b336dba91ef6cb96741b914
Megan <megan@theparkes.me.uk>
2020-07-12 14:05:15 +0100

    speech bubbles

--- src/TeamMember.tsx
+++ src/TeamMember.tsx
@@ -14,11 +14,13 @@ const TeamMember: FunctionComponent<TeamMemberProps> = ({
   image,
 }) => (
   <div className={`team-panel ${className}`}>
-    <img className="team-member-image" src={image} alt="moody" />
-    <div className="flex-item team-member-description">
-      <p>{description}</p>
-      <div className="links flex-container pink">{links}</div>
+    <div className="team-member-wrapper">
+      <img className="team-member-image" src={image} alt="moody" />
+      <div className="flex-item team-member-description">
+        <p>{description}</p>
+      </div>
     </div>
+    <div className="links flex-container pink">{links}</div>
   </div>
 );
 
