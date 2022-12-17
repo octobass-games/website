9f9a9310f753523a4810ab9b39a364c185b3d12a
Megan <megan@theparkes.me.uk>
2022-12-17 17:10:10 +0000

    update hals page

--- src/Halden/HaldenGames.tsx
+++ src/Halden/HaldenGames.tsx
@@ -2,6 +2,13 @@ import React from "react";
 
 const HaldenGames = () => (
   <>
+    <iframe title="Cat got your tongue?" src="https://itch.io/embed/1813240?linkback=true&amp;border_width=0&amp;bg_color=1d1e1e&amp;fg_color=ffffff&amp;link_color=7453af&amp;border_color=d589a4"
+      width="552"
+      height="167"
+      frameBorder="0"
+      className="fade-in fade-in--no-delay itch">
+      <a href="https://octobass.itch.io/cat-got-your-tongue">Cat got your tongue? by octobass, adamskipadamski, majicmoo, Madmouseman, Odettica</a>
+    </iframe>
     <iframe
       src="https://itch.io/embed/1290857?linkback=true&amp;border_width=0&amp;bg_color=d1668c&amp;fg_color=ffffff&amp;link_color=ffffff&amp;border_color=d589a4"
       width="550"
