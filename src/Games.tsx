a99534b1f02d7c163e25c2ec670129df87d2ad37
Megan <megan@theparkes.me.uk>
2025-12-06 17:09:13 +0000

    fix game page

--- src/Games.tsx
+++ src/Games.tsx
@@ -5,25 +5,19 @@ const Games = () => (
   <div className="App mini-games Page">
     <NavBar />
     <hr />
-    <a href="https://octobass.itch.io/dont-bug-me" aria-label="Don't bug me">
-      <div className="DontBugMe game-panel" />
-    </a>
-    <hr />
-    <a href="https://octobass.itch.io/cat-got-your-tongue" aria-label="Cat got your tongue?">
-      <div className="CatGotYourTongue game-panel" />
-    </a>
-    <hr />
-    <a href="https://octobass.itch.io/stumble-and-stew" aria-label="Stumble And Stew">
-      <div className="StumbleAndStew game-panel" />
-    </a>
-    <hr />
-    <a href="https://octobass.itch.io/solander" aria-label="Solander">
-      <div className="Solander game-panel" />
-    </a>
-    <hr />
-    <a href="https://octobass.itch.io/lunar-heist" aria-label="Lunar Heist">
-      <div className="LunarHeist game-panel" />
-    </a>
+    <div className="all-games">
+      <iframe title="Isle of Hronn" frameBorder="0" src="https://itch.io/embed/4084864?border_width=0&amp;bg_color=00d0c3&amp;fg_color=ffffff&amp;link_color=bffdf9&amp;border_color=00b6aa" width="206" height="165"><a href="https://octobass.itch.io/isle-of-hronn">Isle of Hrönn by octobass, majicmoo, Odettica, Madmouseman, adamskipadamski</a></iframe>
+      <iframe title="Don't Bug Me" frameBorder="0" src="https://itch.io/embed/1290857?border_width=0&amp;bg_color=d1668c&amp;fg_color=ffffff&amp;link_color=faf4f6&amp;border_color=b54a70" width="206" height="165"><a href="https://octobass.itch.io/dont-bug-me">Don't Bug Me! by octobass, adamskipadamski, Madmouseman, Odettica, majicmoo</a></iframe>
+
+      <iframe title="Stumble and Stew" frameBorder="0" src="https://itch.io/embed/2398092?border_width=0&amp;bg_color=432952&amp;fg_color=ffffff&amp;link_color=b99dd2&amp;border_color=6a3d84" width="206" height="165"><a href="https://octobass.itch.io/stumble-and-stew">Stumble and Stew by octobass, adamskipadamski, Madmouseman, majicmoo</a></iframe>
+      <iframe title="Solander" frameBorder="0" src="https://itch.io/embed/3149193?border_width=0&amp;bg_color=3f3135&amp;fg_color=c6a8c7&amp;link_color=856087&amp;border_color=67595d" width="206" height="165"><a href="https://octobass.itch.io/solander">Solander by octobass, adamskipadamski, Madmouseman, Odettica, majicmoo</a></iframe>
+
+      <iframe title="Cat got your tongue?" frameBorder="0" src="https://itch.io/embed/1813240?bg_color=8055bb&amp;fg_color=ffffff&amp;link_color=64c5d7&amp;border_color=474646" width="208" height="167"><a href="https://octobass.itch.io/cat-got-your-tongue">Cat got your tongue? by octobass, adamskipadamski, majicmoo, Madmouseman, Odettica</a></iframe>
+
+      <iframe title="Lunar Heist" frameBorder="0" src="https://itch.io/embed/838602?border_width=0&amp;bg_color=2f1d37&amp;fg_color=ff0097&amp;link_color=de0083&amp;border_color=5a4862" width="206" height="165"><a href="https://octobass.itch.io/lunar-heist">
+        Lunar Heist</a>
+      </iframe>
+    </div>
   </div>
 );
 
